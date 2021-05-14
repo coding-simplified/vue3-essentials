c9b831aacdcee5e9624ffcf474c25cc62904dfc6
Mayank Chaudhari <mayank.srmu@gmail.com>
2021-05-14 19:17:08 +0530

    v-for

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 const app = Vue.createApp({
-    data: function() {
+    data: function () {
         return {
-           title: "Graphics Design & Website",
-           img: "/assets/graphics.gif" 
+            serveices: {
+                graphics: {
+                    title: "Graphics Design & Website",
+                    img: "/assets/graphics.gif",
+                    details: ['Graphics with your brand logo', 'Presense on Google Map', 'Delight Your Customers']
+                },
+                mobile: {
+                    title: "Mobile App Developement",
+                    img: "/assets/mobile.gif",
+                    details: ['Android app befitting your brand ', 'Troubleshooting and Debugging', 'App Maintanance ']
+                },
+                cv: {
+                    title: "Computer Vision And Analytics",
+                    img: "/assets/cv1.gif",
+                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla']
+                },
+                consulting: {
+                    title: "Business Consulting",
+                    img: "/assets/vb.jpg",
+                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla']
+                },
+                custom: {
+                    title: "Custom Solutions",
+                    img: "/assets/vb.jpg",
+                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla']
+                }
+            }
         }
     }
 });
