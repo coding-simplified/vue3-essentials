3adcd78badda21080c2491174ac8d993afb40188
Mayank Chaudhari <mayank.srmu@gmail.com>
2021-05-16 12:22:23 +0530

    class-binding

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
 const app = Vue.createApp({
     data: function () {
         return {
-            serveices: {
-                graphics: {
+            cls: 'flx',
+            serveices: [
+                {
                     title: "Graphics Design & Website",
                     img: "/assets/graphics.gif",
                     details: ['Graphics with your brand logo', 'Presense on Google Map', 'Delight Your Customers']
                 },
-                mobile: {
+                {
                     title: "Mobile App Developement",
                     img: "/assets/mobile.gif",
                     details: ['Android app befitting your brand ', 'Troubleshooting and Debugging', 'App Maintanance ']
                 },
-                cv: {
+                {
                     title: "Computer Vision And Analytics",
                     img: "/assets/cv1.gif",
                     details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla']
                 },
-                consulting: {
+                {
                     title: "Business Consulting",
                     img: "/assets/vb.jpg",
                     details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla']
                 },
-                custom: {
+                {
                     title: "Custom Solutions",
                     img: "/assets/custom-cloud.gif",
                     details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla']
                 }
-            }
+            ]
         }
     }
 });
