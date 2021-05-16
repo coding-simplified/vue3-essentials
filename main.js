1e9f5d772378583bb0b72f7156007c40f8dae314
Mayank Chaudhari <mayank.srmu@gmail.com>
2021-05-16 12:29:39 +0530

    added-colors-to-services-object

--- main.js
+++ main.js
@@ -6,27 +6,32 @@ const app = Vue.createApp({
                 {
                     title: "Graphics Design & Website",
                     img: "/assets/graphics.gif",
-                    details: ['Graphics with your brand logo', 'Presense on Google Map', 'Delight Your Customers']
+                    details: ['Graphics with your brand logo', 'Presense on Google Map', 'Delight Your Customers'],
+                    bg: "#1F75A0",
                 },
                 {
                     title: "Mobile App Developement",
                     img: "/assets/mobile.gif",
-                    details: ['Android app befitting your brand ', 'Troubleshooting and Debugging', 'App Maintanance ']
+                    details: ['Android app befitting your brand ', 'Troubleshooting and Debugging', 'App Maintanance '],
+                    bg: "#fff",
                 },
                 {
                     title: "Computer Vision And Analytics",
                     img: "/assets/cv1.gif",
-                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla']
+                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla'],
+                    bg: "#aaa5",
                 },
                 {
                     title: "Business Consulting",
                     img: "/assets/vb.jpg",
-                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla']
+                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla'],
+                    bg: "#fff",
                 },
                 {
                     title: "Custom Solutions",
                     img: "/assets/custom-cloud.gif",
-                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla']
+                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla'],
+                    bg: "#4279E8",
                 }
             ]
         }
