63225eb8f6d7abf26290c3877adb8f546eb7a3b1
Mayank Chaudhari <mayank.srmu@gmail.com>
2021-05-29 13:27:09 +0530

    add-slide-images

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+const slides = [
+    {
+        title: 'Business Consulting',
+        description: 'We have partnered with Dr. Vivek Bindra and Bada Business to empower youth, small businesses, MSMEs and SMEs with the most affordable and high quality entrepreneurial Skills & Knowledge.',
+        images: ['/assets/vb.jpg', '/assets/vb1.jpg', '/assets/vb2.jpg',]
+    },
+    {
+        title: '100% Customer Satisfaction',
+        description: 'We put customer satisfaction at highest priority and thanks to all our wonderful customers that we are able to maintain 100% 5✯ rating on Upwork so far!',
+        images: ['/assets/cs.jpg', '/assets/cs.svg', '/assets/cs1.jpg', '/assets/cs2.jpg']
+    },
+    {
+        title: 'Mobile And Web Development',
+        description: 'We deliver high quality Apps, Websites, Softwares and Scripts. Thanks to all our wonderful customers that we are able to maintain 100% 5✯ rating on Upwork so far!',
+        images: ['/assets/m.jpg', '/assets/m1.jpg', '/assets/m1.png']
+    }
+]
+
 const app = Vue.createApp({
     data: function () {
         return {
