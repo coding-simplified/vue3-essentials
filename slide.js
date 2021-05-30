744692f7410dc0675aa05c005ad4a21073bd3e2f
Mayank Chaudhari <mayank.srmu@gmail.com>
2021-05-30 23:12:22 +0530

    title_animation

--- slide.js
+++ slide.js
@@ -2,9 +2,15 @@ const slide = {
     template: /*html*/ `
     <div class="slide" :style="{backgroundImage: 'url('+ slide.images[0] + ')'}">
                 <span class="spacer"></span>
+                <transition name="slide-right" appear>
                 <h1>{{slide.title}}</h1>
+                </transition>
+                <transition name="slide-left" appear>
                 <p>{{slide.description}}</p>
+                </transition>
+                <transition name="slide-up" appear>
                 <button class="btn" @click="getInTouch()">GET IN TOUCH</button>
+                </transition>
                 <span class="spacer"></span>
             </div>
     `,
